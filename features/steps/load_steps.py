7eb7613d9153b01538443b73a5e6e5707da01943
andresFLZ <limaszandresfelipe@gmail.com>
2023-12-14 09:32:10 -0500

    trabajo final

--- features/steps/load_steps.py
+++ features/steps/load_steps.py
@@ -23,12 +23,24 @@ For information on Waiting until elements are present in the HTML see:
     https://selenium-python.readthedocs.io/waits.html
 """
 import requests
-from behave import given
+from behave import *
 
 # HTTP Return Codes
 HTTP_200_OK = 200
 HTTP_201_CREATED = 201
 HTTP_204_NO_CONTENT = 204
+
+def before_scenario(context, scenario):
+    for row in context.table:
+        payload = {
+            "name": row['name'],
+            "description": row['description'],
+            "price": row['price'],
+            "available": row['available'] in ['True', 'true', '1'],
+            "category": row['category']
+        }
+        context.resp = requests.post(rest_endpoint, json=payload)
+        assert context.resp.status_code == HTTP_201_CREATED
 
 @given('the following products')
 def step_impl(context):
@@ -43,10 +55,10 @@ def step_impl(context):
         context.resp = requests.delete(f"{rest_endpoint}/{product['id']}")
         assert(context.resp.status_code == HTTP_204_NO_CONTENT)
 
-    #
+    """#
     # load the database with new products
     #
     for row in context.table:
-        #
-        # ADD YOUR CODE HERE TO CREATE PRODUCTS VIA THE REST API
-        #
+    #
+    # ADD YOUR CODE HERE TO CREATE PRODUCTS VIA THE REST API
+    #"""
